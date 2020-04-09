99724b03afb1e3aec8ba71853c644e1e2061e304
Catherine Price <catprice444@gmail.com>
2020-04-09 01:03:57 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -20,7 +20,7 @@ end
 
 
 def valid_move(board,index)
-  if index. between?(0,8) && !position_taken?(board, index)
+  if index.between?(0,8) && !position_taken?(board, index)
     returns true
   end
 end
