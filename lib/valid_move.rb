6b319b6a2bb119aceab755765eb4a87a4a6eba6b
Catherine Price <catprice444@gmail.com>
2020-04-09 00:49:59 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -2,6 +2,12 @@
 
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
+
+#need to confirm that the position on the board is valid and that is it not taken
+
+
+between? (0, 8)
+
 def position_taken?(board, index)
    if (board[index] == " ") || (board[index] == "") || (board[index] == nil)
       return false
